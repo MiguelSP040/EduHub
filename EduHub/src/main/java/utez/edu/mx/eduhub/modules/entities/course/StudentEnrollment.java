25449e90397fdf261cb08961e8e0411ecc582f62
AlexReyes03 <20233TN160@utez.edu.mx>
2025-03-29 02:17:46 -0600

    [UPDATE] Implementada la vista de calificaciones, ahora el estudiante inscrito manda su fecha de inscripción

--- EduHub/src/main/java/utez/edu/mx/eduhub/modules/entities/course/StudentEnrollment.java
+++ EduHub/src/main/java/utez/edu/mx/eduhub/modules/entities/course/StudentEnrollment.java
@@ -1,11 +1,13 @@
 package utez.edu.mx.eduhub.modules.entities.course;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class StudentEnrollment {
     private String studentId;
     private String status; // "Pendiente", "Aceptado", "Rechazado"
+    private Date enrolledDate;
     private boolean certificateDelivered = false;
     private String certificateFile;
     private List<String> completedSessions;
@@ -16,6 +18,7 @@ public class StudentEnrollment {
         this.completedSessions = new ArrayList<>();
         this.certificateDelivered = false;
         this.certificateFile = null;
+        this.enrolledDate = new Date();
     }
 
     public int calculateProgress(int totalSessions) {
@@ -37,6 +40,14 @@ public class StudentEnrollment {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public Date getEnrolledDate() {
+        return enrolledDate;
+    }
+
+    public void setEnrolledDate(Date enrolledDate) {
+        this.enrolledDate = enrolledDate;
     }
 
     public boolean isCertificateDelivered() {
